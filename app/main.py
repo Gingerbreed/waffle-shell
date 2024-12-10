050b820d6fff6cdce390c85c03c060ca12bcb165
Morgan Baker <morgan@mbaker.org>
2024-12-09 21:54:21 -0500

    trying repl loop

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-
-    # Wait for user input
-    #input()
-    command = input()
-    sys.stdout.write(command + ": command not found\n")
+    i = 1
+    while i > 1000:
+        sys.stdout.write("$ ")
+        command = input()
+        sys.stdout.write(command + ": command not found\n")
+        i += 1
+        
     
 
 if __name__ == "__main__":
