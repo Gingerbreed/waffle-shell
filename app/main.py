cec55818aea0e9cf8c6fb873d42693a2eb8b6791
Morgan Baker <morgan@mbaker.org>
2024-12-11 06:21:15 -0500

    why can't i test this?

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-import sys
 import os
 import subprocess
+import sys
 from typing import Optional
-
 def locate_executable(command) -> Optional[str]:
     path = os.environ.get("PATH", "")
     for directory in path.split(":"):
@@ -10,32 +9,40 @@ def locate_executable(command) -> Optional[str]:
         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
             return file_path
 
+def handle_exit(args):
+    sys.exit(int(args[0]) if args else 0)
+
+def handle_echo(args):
+    print(" ".join(args))
+
+def handle_type(args):
+    if args[0] in builtins:      
+        print(f"{args[0]} is a shell builtin")
+    elif executable := locate_executable(args[0]):
+        print(f"{args[0]} is {executable}")
+    else:
+        print(f"{args[0]} not found")
+
+        
+builtins = {
+    "exit": handle_exit,
+    "echo": handle_echo,
+    "type": handle_type,
+}
+
 def main():
-    validcommands = ["echo", "exit", "type"]
-    PATH = os.environ.get("PATH")
     while True:
         sys.stdout.write("$ ")
-        response = input()
-        match response.split(" "):
-            case ["exit", "0"]:
-                exit(0)
-            case ["echo", *cmd]:
-                print(" ".join(cmd))
-            case ["type", *cmd]:
-                match cmd:
-                    case ["echo" | "exit" | "type"]:
-                        print(f"${cmd[0]} is a shell builtin")
-                    case _:
-                        if executable := locate_executable(args[0]):
-                            print(f"{cmd} is {executable}")    
-                        else:
-                            print(response[5:] + ": not found")
-            case _:
-                if executable := locate_executable(command):
-                    subprocess.run([command, response.split(" ")[1]])
-                print(f"{response}: command not found")
-
-    
+        sys.stdout.flush()
+        command, *args = input().split(" ")
+        if command in builtins:
+            builtins[command](args)
+            continue
+        elif executable := locate_executable(command):
+            subprocess.run([executable, *args])
+        else:
+            print(f"{command}: command not found")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
