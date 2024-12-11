2d9c748b4bdd8790322262091564dc798da3a47a
Morgan Baker <morgan@mbaker.org>
2024-12-11 05:28:16 -0500

    adding file paths

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import sys
+import os
 
 def main():
     validcommands = ["echo", "exit", "type"]
+    PATH = os.environ.get("PATH")
     i = 1
     while bool(i):
         sys.stdout.write("$ ")
@@ -10,13 +12,23 @@ def main():
         # if command in validcommands:
         if "exit" in command:
             break
-        elif "echo" in command:
+        if "echo" in command:
             print(response[5:])
-        elif "type" in command:
+            continue
+        if "type" in command:
+            cmd = response.split(" ")[1]
+            cmd_path = None
+            paths = PATH.split(":")
+            for path in paths:
+                if os.path.isfile(f"{path}/{cmd}"):
+                    cmd_path = f"{path}/{cmd}"
             if response[5:] in validcommands:
-                print(response[5:] + " is a shell builtin")   
+                print(response[5:] + " is a shell builtin")
+            elif cmd_path:
+                print(f"{cmd} is {cmd_path}") 
             else:
                 print(response[5:] + ": not found")
+            continue
         else:
             print(command + ": command not found")
 
