4307c3016777b8c005d42f4dce581d8e15090f8f
Morgan Baker <morgan@mbaker.org>
2024-12-11 05:00:45 -0500

    add type builtin and command matrix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import sys
 
 def main():
+    validcommands = ["echo", "exit", "type"]
     i = 1
     while bool(i):
         sys.stdout.write("$ ")
         response = input()
         command = response.split()[0]
-        if "exit" in command:
-            break
-        if "echo" in command:
-            print(response[5:])
+        if command in validcommands:
+            if "exit" in command:
+                break
+            if "echo" in command:
+                print(response[5:])
+            if "type" in command:
+                if response[5:] in validcommands:
+                    print(response[5:] + " is a shell builtin\n")   
+                else:
+                    print(response[5:] + ": command not found\n")
+
         else:
             sys.stdout.write(command + ": command not found\n")
 
